4c3f202a25a3ff9211a4dd9c52b2c5e05318b934
davidwilson826 <davidwilson@hanovernorwichschools.org>
2016-03-12 13:43:52 -0500

    Updated from Brython Server: 3/12/2016 1:43:50 PM

--- AppVarTest.py
+++ AppVarTest.py
@@ -1,9 +1,24 @@
-from ggame import App, Sprite, ImageAsset, Frame
+from ggame import App, Sprite, CircleAsset, Line, Color, Frame
+
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 600
+
+black = Color(0)
+thinline = Line(0, 1)
+
+class Player(Sprite):
+    
+    asset = CircleAsset(10, black, thinline)
+    
+    def __init__(self, position):
+        super().__init__(Player.asset, position)
 
 class SpaceWar(App):
     
     def __init__(self):
         super().__init__()
+        self.playerRot = 0
+        Player((SCREEN_WIDTH/2, SCREEN_HEIGHT/2))
 
 app = SpaceWar()
 app.run()
