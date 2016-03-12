c97ebd43909a2dab01f67d9303f84c25c67f1fd5
davidwilson826 <davidwilson@hanovernorwichschools.org>
2016-03-12 16:03:51 -0500

    Updated from Brython Server: 3/12/2016 4:03:50 PM

--- MoveTest.py
+++ MoveTest.py
@@ -1,4 +1,4 @@
-from ggame import App, Sprite, LineStyle, Color, PolygonAsset, CircleAsset
+from ggame import App, Sprite, LineStyle, Color, PolygonAsset, CircleAsset, RectangleAsset
 
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 600
@@ -10,26 +10,47 @@ class Thing(Sprite):
     
     #asset = PolygonAsset([(0,100), (25,0), (50,100)], thinline, black)
     asset = CircleAsset(25, thinline, black)
+    #asset = RectangleAsset(50, 20, thinline, black)
     
     def __init__(self, position):
         super().__init__(Thing.asset, position)
         self.rotSpd = 0.1
+        self.dist = 0
+        self.go = "false"
 #        self.fxcenter = 0.5
 #        self.fycenter = 0.5
         ThingMove.listenKeyEvent("keydown", "right arrow", self.rotateRight)
         ThingMove.listenKeyEvent("keydown", "left arrow", self.rotateLeft)
+        ThingMove.listenKeyEvent("keydown", "space", self.go)
         
     def rotateRight(self, event):
         self.rotation -= self.rotSpd
         
     def rotateLeft(self, event):
         self.rotation += self.rotSpd
+        
+    def go(self, event):
+        self.go = "true"
+        
+    def step(self):
+        if self.dist < 50 and self.go == "true":
+            self.x += 5
+            self.y += 5
+            self.dist += 5
+        else:
+            self.go = "false"
+            self.dist = 0
     
 class ThingMove(App):
     
     def __init__(self):
         super().__init__()
         Thing((SCREEN_WIDTH/2, SCREEN_HEIGHT/2))
+        self.step()
+        
+    def step(self):
+        for x in self.getSpritesbyClass(Thing):
+            x.step()
 
 app = ThingMove()
 app.run()
