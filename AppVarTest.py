d4df3e8ef17c5d43d9d7a6a2fb9777b8cee37c8a
davidwilson826 <davidwilson@hanovernorwichschools.org>
2016-03-12 13:59:52 -0500

    Updated from Brython Server: 3/12/2016 1:59:51 PM

--- AppVarTest.py
+++ AppVarTest.py
@@ -12,13 +12,30 @@ class Player(Sprite):
     
     def __init__(self, position):
         super().__init__(Player.asset, position)
+        
+    def step(self):
+        self.rotation = self.app.playerRot
 
 class SpaceWar(App):
     
     def __init__(self):
         super().__init__()
         self.playerRot = 0
+        self.rotSpd = 0.1
         Player((SCREEN_WIDTH/2, SCREEN_HEIGHT/2))
+        SpaceGame.listenKeyEvent("keydown", "right arrow", self.rotateRight)
+        SpaceGame.listenKeyEvent("keydown", "left arrow", self.rotateLeft)
+        self.step()
+        
+    def rotateRight(self, event):
+        self.playerRot -= self.rotSpd
+    
+    def rotateLeft(self, event):
+        self.playerRot += self.rotSpd
+        
+    def step(self):
+        for x in self.getSpritesbyClass(Player):
+            x.step()
 
 app = SpaceWar()
 app.run()
