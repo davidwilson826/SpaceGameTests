5fa64cfb1c42033680a4dd1ccf985cc7e14bcd0a
davidwilson826 <davidwilson@hanovernorwichschools.org>
2016-03-12 15:40:24 -0500

    Updated from Brython Server: 3/12/2016 3:40:23 PM

--- MoveTest.py
+++ MoveTest.py
@@ -10,5 +10,14 @@ class Thing(Sprite):
     
     asset = PolygonAsset([(0,100), (25,0) (50,100)], thinline, black)
     
-app = SpaceWar()
+    def __init__(self, position):
+        super().__init__(Thing.asset, position)
+    
+class ThingMove(App):
+    
+    def __init__(self):
+        super().__init__()
+        Thing((SCREEN_WIDTH/2, SCREEN_HEIGHT/2))
+
+app = ThingMove()
 app.run()
