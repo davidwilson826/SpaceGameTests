2c783d8b76681ff9124d4435982de535d43a54b7
davidwilson826 <davidwilson@hanovernorwichschools.org>
2016-04-08 13:32:15 -0400

    Updated from Brython Server: 4/8/2016 1:32:15 PM

--- StarBack.py
+++ StarBack.py
@@ -1,17 +1,16 @@
 from ggame import App, Sprite, ImageAsset, Frame, Color
 
-SCREEN_WIDTH = 1000
-SCREEN_HEIGHT = 600
+SCREEN_WIDTH = 512
+SCREEN_HEIGHT = 512
 
 class StarBack(Sprite):
     
-    asset = ImageAsset("images/starfield.jpg", Frame(0,0,SCREEN_WIDTH,SCREEN_HEIGHT))
+    asset = ImageAsset("images/starfield.jpg", Frame(0,0,512,512*(SCREEN_HEIGHT/SCREEN_WIDTH)))
     
     def __init__(self, position):
         super().__init__(StarBack.asset, position)
         
 class Test(App):
   StarBack((0,0))
-  
-myapp = Test()
-myapp.run()
+
+Test().run()
