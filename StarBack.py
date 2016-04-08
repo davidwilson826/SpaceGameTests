0ca252479dca08f43e351b00aa7e484de34ddfd5
davidwilson826 <davidwilson@hanovernorwichschools.org>
2016-04-08 14:05:14 -0400

    Update StarBack.py

--- StarBack.py
+++ StarBack.py
@@ -1,16 +1,29 @@
-from ggame import App, Sprite, ImageAsset, Frame, Color
+from ggame import App, Sprite, ImageAsset, Frame, Color, CircleAsset, LineStyle
 
-SCREEN_WIDTH = 512
-SCREEN_HEIGHT = 512
+SCREEN_WIDTH = 400
+SCREEN_HEIGHT = 600
+
+if SCREEN_WIDTH >= SCREEN_HEIGHT:
+    LARGER_SIDE = SCREEN_WIDTH
+    SMALLER_SIDE = SCREEN_HEIGHT
+elif SCREEN_HEIGHT > SCREEN_WIDTH:
+    LARGER_SIDE = SCREEN_HEIGHT
+    SMALLER_SIDE = SCREEN_WIDTH
+
+white = Color(0xffffff, 1.0)
+
+line = LineStyle(1.0, white)
 
 class StarBack(Sprite):
     
-    asset = ImageAsset("images/starfield.jpg", Frame(0,0,512,512*(SCREEN_HEIGHT/SCREEN_WIDTH)))
+    asset = ImageAsset("images/starfield.jpg", Frame(0,0,512*(SMALLER_SIDE/LARGER_SIDE),512))
     
     def __init__(self, position):
         super().__init__(StarBack.asset, position)
+        self.scale = LARGER_SIDE/512
         
 class Test(App):
   StarBack((0,0))
+  Sprite(CircleAsset(50, line, white), (SCREEN_WIDTH, SCREEN_HEIGHT))
 
 Test().run()
