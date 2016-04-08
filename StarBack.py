6df72975964055fe6d662f6c7a0db9cd8f9594c4
davidwilson826 <davidwilson@hanovernorwichschools.org>
2016-04-08 12:57:58 -0400

    Updated from Brython Server: 4/8/2016 12:57:58 PM

--- StarBack.py
+++ StarBack.py
@@ -5,13 +5,13 @@ SCREEN_HEIGHT = 600
 
 class StarBack(Sprite):
     
-    asset = ImageAsset("images/starfield.jpg", Frame(0,0,SCREEN_WIDTH/2,SCREEN_HEIGHT/2))
+    asset = ImageAsset("images/starfield.jpg", Frame(0,0,SCREEN_WIDTH,SCREEN_HEIGHT))
     
     def __init__(self, position):
         super().__init__(StarBack.asset, position)
-        self.scale = 2
         
 class Test(App):
   StarBack((0,0))
   
-Test.run()
+myapp = Test()
+myapp.run()
