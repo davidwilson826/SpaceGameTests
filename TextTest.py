80a28731a1f8f47db3bd59c763ffa3389f037a54
davidwilson826 <davidwilson@hanovernorwichschools.org>
2016-04-09 19:50:49 -0400

    Updated from Brython Server: 4/9/2016 7:50:47 PM

--- TextTest.py
+++ TextTest.py
@@ -3,3 +3,21 @@ from ggame import Color, TextAsset, Sprite, App
 white = Color(0xffffff, 1.0)
 
 class WhiteText(TextAsset):
+    
+    def __init__(self, text):
+        super().__init__(text)
+        
+class ToPrint(Sprite):
+    
+    asset = WhiteText('Hello')
+    
+    def __init__(self, position):
+        super().__init__(ToPrint.asset, position)
+        
+class Print(App):
+    
+    def __init__(self):
+        super().__init__()
+        ToPrint((100,100))
+        
+Print().run()
