f73387777f6694574d56dd45ef123cc0802d45d5
davidwilson826 <davidwilson@hanovernorwichschools.org>
2016-04-10 17:37:43 -0400

    Updated from Brython Server: 4/10/2016 5:37:43 PM

--- MoveTest2.py
+++ MoveTest2.py
@@ -8,21 +8,32 @@ class Player(Sprite):
         
     def __init__(self, position):
         super().__init__(Player.asset, position)
+        self.velocity = (0,0)
         SpaceGame.listenKeyEvent("keydown", "right arrow", self.rotateRight)
         SpaceGame.listenKeyEvent("keydown", "left arrow", self.rotateLeft)
-        SpaceGame.listenKeyEvent("keydown", "up arrow", self.thrustOn)
-        SpaceGame.listenKeyEvent("keyup", "up arrow", self.thrustOff)
+#        SpaceGame.listenKeyEvent("keydown", "up arrow", self.thrustOn)
+#        SpaceGame.listenKeyEvent("keyup", "up arrow", self.thrustOff)
         
     def rotateRight(self, event):
         self.rotation -= self.rotSpd
         
     def rotateLeft(self, event):
         self.rotation += self.rotSpd
+        
+#    def thrustOn(self, event):
+
+    def step(self):
+        self.x += self.velocity[0]
+        self.y += self.velocity[1]
 
 class SpaceGame(App):
     
     def __init__(self):
         super().__init__()
         Player((100,100))
+        
+    def step(self):
+        for x in self.getSpritesbyClass(Player):
+            x.step()
 
 SpaceGame().run()
