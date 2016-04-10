f6ab1f24b772f82b4166204bd848d28527994f85
davidwilson826 <davidwilson@hanovernorwichschools.org>
2016-04-10 18:56:45 -0400

    Updated from Brython Server: 4/10/2016 6:56:45 PM

--- MoveTest2.py
+++ MoveTest2.py
@@ -11,7 +11,7 @@ class Player(Sprite):
         self.fxcenter = self.fycenter = 0.5
         self.velocity = (0,0)
         self.rotSpd = 0.1
-        self.magnitude = 0.5
+        self.magnitude = 0.25
         SpaceGame.listenKeyEvent("keydown", "right arrow", self.rotateRight)
         SpaceGame.listenKeyEvent("keydown", "left arrow", self.rotateLeft)
         SpaceGame.listenKeyEvent("keydown", "up arrow", self.thrust)
