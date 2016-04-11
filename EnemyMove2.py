d4a3687e486d393b069da399a1729e03fdd0f3cb
davidwilson826 <davidwilson@hanovernorwichschools.org>
2016-04-11 09:16:18 -0400

    Updated from Brython Server: 4/11/2016 9:16:16 AM

--- EnemyMove2.py
+++ EnemyMove2.py
@@ -1,5 +1,6 @@
 from ggame import App, Sprite, ImageAsset, Frame
-form math import sin, cos
+from math import sin, cos, pi
+from random import randint
 
 class Enemy(Sprite):
     
@@ -9,11 +10,24 @@ class Enemy(Sprite):
     def __init__(self, position):
         super().__init__(Enemy.asset, position)
         self.fxcenter = self.fycenter = 0.5
+        self.velocity = (0,0)
+        self.magnitude = 0.25
+        
+    def step(self):
+        if randint(0,100) == 0:
+            self.rotation = randint(0,1000)/500*pi
+            self.velocity = (sum(x) for x in zip(self.velocity, (-1*self.magnitude*sin(self.rotation),
+            -1*self.magnitude*cos(self.rotation))))
+        self.x += self.velocity[0]
+        self.y += self.velocity[1]
         
 class SpaceGame(App):
 
     def __init__(self):
         super().__init__()
         Enemy((500,300))
+        
+    for x in self.getSpritesbyClass(Enemy):
+        x.step()
         
 SpaceGame().run()
